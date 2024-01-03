a58e1a1354d84bd53ae1c2caabed9bd9b8373127
carisaelam <carisaelam@gmail.com>
2024-01-02 20:48:53 -0500

    eep

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
+import Message from "./Message/Message";
 
 function App() {
   return (
     <div>
       <h1>Hello there CRA. </h1>
+      <Message />
     </div>
   );
 }
---
+++ src/Message/Message.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Message.css";
+
+function Message() {
+  return <h1>MY MESSAGE</h1>;
+}
+
+export default Message;
